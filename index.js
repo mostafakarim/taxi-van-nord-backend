d3768754ddc833ef39f416bb2ce4be511beb2044
mostafakarim <mostafa.karim@live.fr>
2024-03-19 17:36:42 +0100

    feat(vercel): add vercel json

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./api/index');
+var usersRouter = require('./api/users');
 const cors = require('cors')
 var app = express();
 app.use(cors())
